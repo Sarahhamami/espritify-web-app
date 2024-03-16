78159cc5d320d2640bec704e2e94711c58b255b2
Sarah Hammami <92446749+Sarahhamami@users.noreply.github.com>
2024-03-17 00:06:12 +0100

    meeting and meeting details front-office

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -15,4 +15,18 @@ class AccueilController extends AbstractController
             'controller_name' => 'AccueilController',
         ]);
     }
+    #[Route('/meetings', name: 'app_meetings')]
+    public function meetings(): Response
+    {
+        return $this->render('accueil/meetings.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+    #[Route('/meeting-details', name: 'app_meeting-details')]
+    public function meeting_details(): Response
+    {
+        return $this->render('accueil/meeting-details.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
 }
